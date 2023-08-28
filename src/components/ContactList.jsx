1c397245fa55cf8899dcd7fa0e6218f6fde383a7
tomkjose <tomjosekunnumpurath@gmail.com>
2023-08-28 10:08:13 +0530

    latests commits

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ContactCard from "../components/ContactCard";
 import Pagination from "@mui/material/Pagination";
 import "../styles/app.css";
 function ContactList({ contactList, deleteContact }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+  const lastIndex = currentPage * itemsPerPage;
+  const firstIndex = lastIndex - itemsPerPage;
+  const currentContacts = contactList.slice(firstIndex, lastIndex);
+
+  const handlePageChange = (event, page) => {
+    setCurrentPage(page);
+  };
   return (
     <div className="ContactList">
-      {contactList.map((contact) => {
+      {currentContacts.map((contact) => {
+        console.log("currentContacts", currentContacts);
         return (
           <div style={{ width: "100%" }} key={contact._id}>
             <ContactCard contact={contact} deleteContact={deleteContact} />
           </div>
         );
       })}
-      <Pagination count={10} color="secondary" className="Pagination" />
+      <Pagination
+        count={Math.ceil(contactList.length / itemsPerPage)}
+        color="secondary"
+        className="Pagination"
+        onChange={handlePageChange}
+      />
     </div>
   );
 }
