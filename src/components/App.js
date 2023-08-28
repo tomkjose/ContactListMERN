416ec38c2a8a36ef1bc27bcf5c78ce26138b5e7c
tomkjose <tomjosekunnumpurath@gmail.com>
2023-08-28 10:09:31 +0530

    third commits

--- src/components/App.js
+++ src/components/App.js
@@ -103,7 +103,7 @@ function App() {
           +
         </Button>
       </div>
-      <SearchBar />
+      {/* <SearchBar /> */}
       <ContactList contactList={contactList} deleteContact={deleteContact} />
     </div>
   );
